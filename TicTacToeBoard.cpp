0e6e3b50cf8a1a9fd75be72d2e6500499abcd97c
Austin D Bruch <adbruch@ecc-jaguar.csuchico.edu>
2019-11-22 12:37:24 -0800

    changed loop location and how returns work in winner check

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -79,33 +79,31 @@ Piece TicTacToeBoard::getWinner()
 {
   Piece winner = Blank;
   //Check if game is over or not.
+  
+  //vertical wins
+  if (board[0][0] == board[0][1] == board[0][2])
+    return board[0][0];
+  else if (board[1][0] == board[1][1] == board[1][2])
+    return board[1][0];
+  else if (board[2][0] == board[2][1] == board[2][2])
+    return board[2][0];
+  //horizontal wins
+  else if (board[0][0] == board[1][0] == board[2][0])
+    return board[0][0];
+  else if (board[0][1] == board[1][1] == board[2][1])
+    return board[0][1];
+  else if (board[0][2] == board[1][2] == board[2][2])
+    return board[0][2];
+  //diagonal wins
+  else if (board[0][0] == board[1][1] == board[2][2])
+    return board[0][0];
+  else if (board[0][2] == board[1][1] == board[2][0])
+    return board[0][2];
+  else
+    return Blank;
   for(int i=0; i<BOARDSIZE; i++)
     for(int j=0; j<BOARDSIZE; j++)
       if (board[i][j] == Blank)
         return Invalid;
-  //vertical wins
-  if (board[0][0] == board[0][1] == board[0][2])
-    winner = board[0][0];
-  else if (board[1][0] == board[1][1] == board[1][2])
-    winner = board[1][0];
-  else if (board[2][0] == board[2][1] == board[2][2])
-    winner = board[2][0];
-  //horizontal wins
-  else if (board[0][0] == board[1][0] == board[2][0])
-    winner = board[0][0];
-  else if (board[0][1] == board[1][1] == board[2][1])
-    winner = board[0][1];
-  else if (board[0][2] == board[1][2] == board[2][2])
-    winner = board[0][2];
-  //diagonal wins
-  else if (board[0][0] == board[1][1] == board[2][2])
-    winner = board[0][0];
-  else if (board[0][2] == board[1][1] == board[2][0])
-    winner = board[0][2];
-  else
-    return Blank;
   return winner;
-
-  
-
 }
